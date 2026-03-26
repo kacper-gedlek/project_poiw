2efc6052558b70f0462b177e1e340a83771ecd05
zzaskroniec <fgadekxyz@icloud.com>
2026-03-26 14:45:25 +0100

    readerUtils: unit tests added

--- readerUtils/src/test/java/poiw/project/CAS/ReaderTest.java
+++ readerUtils/src/test/java/poiw/project/CAS/ReaderTest.java
@@ -1,10 +1,30 @@
 package poiw.project.CAS;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class LibraryTest {
-	@Test void testReader() {
-		
-	}
+class ReaderTest {
+
+    private Reader reader;
+
+    @BeforeEach
+    void setUp() {
+        reader = new Reader();
+        reader.initialize();
+    }
+
+    @Test
+    @DisplayName("Should initialize and return reader list")
+    void testInitializeAndGetReaders() {
+        var readers = reader.getReaders();
+        assertNotNull(readers, "Reader list should not be null");
+    }
+
+    @Test
+    @DisplayName("Should return empty UID when no reader is selected")
+    void testGetUIDWithoutSelection() {
+        String uid = reader.getUID();
+        assertEquals("", uid, "UID should be empty string when no reader is selected");
+    }
 }
+
